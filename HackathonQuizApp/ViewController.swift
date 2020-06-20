c090e053dbef317afe73a8758e438cb054d75c16
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2020-06-20 14:20:48 -0700

    Initial Commit

---
+++ HackathonQuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackathonQuizApp
+//
+//  Created by Ari Stassinopoulos on 6/20/20.
+//  Copyright © 2020 Ari Stassinopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
